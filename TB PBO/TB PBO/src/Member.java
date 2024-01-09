e9642669cd0b73ad08ef769c2d7566443a363f1e
RyanAlGhifari2211533014 <145088178+RyanAlGhifari2211533014@users.noreply.github.com>
2024-01-09 19:52:52 +0700

    Add files via upload

---
+++ TB PBO/TB PBO/src/Member.java
@@ -0,0 +1,15 @@
+    public abstract class Member {
+        protected Integer poin = 0;
+
+        String namaPelanggan;
+        String kodemenu;
+        String namamenu;
+        String noHp;
+        String alamat;
+        String namaKasir="Koji";
+        Long hargamakanan;
+        Long jumlahBeli;
+        Long totalBayar;
+        
+        
+}
